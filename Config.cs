06dde2d858af435f567ccd8caf8f51eed502ac2e
Masahiro Yoshida <myoshida@aiming-inc.com>
2021-10-13 15:54:08 +0900

    useful config file

--- Config.cs
+++ Config.cs
@@ -1,5 +1,4 @@
-// unset
-
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -25,27 +24,81 @@ namespace Shtxt
     }
     public class Config
     {
+        public List<DirectoryInfo> InputFiles { get; set; }
         public string OutputDir { get; set; } = ".";
-        public NewLine NewLine { get; set; } = NewLine.LF;
+        public string NewLine { get; set; } = "LF";
 
-        public void SetNewLine(string str)
-        {
-            NewLine l = NewLine.LF;
-            NewLine.TryParse(str, true, out l);
-            this.NewLine = l;
-        }
-        public TextFormat TextFormat { get; set; } = TextFormat.Tsv;
+        // public void SetNewLine(string str)
+        // {
+        //     NewLine l = NewLine.LF;
+        //     NewLine.TryParse(str, true, out l);
+        //     this.NewLine = l;
+        // }
+        public string TextFormat { get; set; } = "tsv";
 
-        public void SetTextFormat(string str)
-        {
-            TextFormat t;
-            TextFormat.TryParse(str, true, out t);
-            this.TextFormat = t;
-        }
+        // public void SetTextFormat(string str)
+        // {
+        //     TextFormat t;
+        //     TextFormat.TryParse(str, true, out t);
+        //     this.TextFormat = t;
+        // }
         public string CommentStartsWith { get; set; } = "#";
         public string TableNameTag { get; set; } = "[テーブル名]";
         public string ColumnControlTag { get; set; } = "[カラム制御]";
         public string ColumnNameTag { get; set; } = "[カラム名]";
+        
+        public FileInfo ConfigFile { get; set; }
+
+        private void SetByKeyValue(string key, string value)
+        {
+            if (key == "InputFiles")
+            {
+                if (InputFiles == null) InputFiles = new List<DirectoryInfo>();
+                
+                var files = value.Split(" ");
+                foreach (var file in files)
+                {
+                    InputFiles.Add( new DirectoryInfo(file));
+                }
+
+                return;
+            }
+            var propertyInfo = this.GetType().GetProperty(key);
+            if (propertyInfo == null) return;
+            propertyInfo.SetValue(this, value);
+        }
+        public void LoadFromFile()
+        {
+            var configFileList = new List<string>() {"config.yml","config.yaml", "Config.yml", "Config.yaml"};
+            string path = null;
+            if (ConfigFile != null && ConfigFile.Exists)
+            {
+                path = ConfigFile.FullName;
+            }
+            else
+            {
+                foreach (var file in configFileList)
+                {
+                    if (File.Exists(file))
+                    {
+                        path = file;
+                        break;
+                    }
+                }
+            }
+
+            if (String.IsNullOrEmpty(path)) return;
+            
+            using (var reader = new StreamReader(path, Encoding.UTF8))
+            {
+                var deserializer = new DeserializerBuilder().Build();
+                var dict = deserializer.Deserialize<Dictionary<string, string>>(reader.ReadToEnd());
+                foreach (var kv in dict)
+                {
+                    SetByKeyValue(kv.Key, kv.Value);
+                }
+            }
+        }
 
         public static Config LoadFromFile(string path)
         {
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -35,11 +36,11 @@ namespace Shtxt
             var separator = "";
             switch (config.TextFormat)
             {
-                case TextFormat.Csv:
+                case "csv":
                     ext = ".csv";
                     separator = ",";
                     break;
-                case TextFormat.Tsv:
+                case "tsv":
                     ext = ".tsv";
                     separator = "\t";
                     break;
@@ -51,13 +52,13 @@ namespace Shtxt
             {
                 switch (config.NewLine)
                 {
-                    case NewLine.CR:
+                    case "CR":
                         writer.NewLine = "\r";
                         break;
-                    case NewLine.LF:
+                    case "LF":
                         writer.NewLine = "\n";
                         break;
-                    case NewLine.CRLF:
+                    case "CRLF":
                         writer.NewLine = "\r\n";
                         break;
                 }
@@ -66,9 +67,23 @@ namespace Shtxt
             }
         }
 
-        static void Convert(IList<string> inputPaths, Config config)
+        static void Convert(Config config)
         {
-            var tasks = inputPaths.SelectMany(path =>
+            var tasks = config.InputFiles.SelectMany(info =>
+            {
+                if ((info.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    return Directory.GetFiles(info.FullName, "*.xlsx", SearchOption.AllDirectories);
+                }
+                else
+                {
+                    if (info.FullName.EndsWith(".xlsx"))
+                    {
+                        return new string[] {info.FullName};
+                    }
+                    return new string[] {};
+                }
+            }).SelectMany(path =>
             {
                 var book = WorkbookFactory.Create(path, password: null, readOnly: true);
                 return book.GetSheetEnumerable();
@@ -88,65 +103,21 @@ namespace Shtxt
         static void Main(string[] args)
         {
             var command = new RootCommand();
-            command.Add(new Argument<List<FileInfo>>("input-files"));
+            command.Add(new Argument<List<DirectoryInfo>>("input-files"));
             command.Add(new Option<string>(new string[] {"-o", "--output-dir"}, "output directory"));
             command.Add(new Option<string>(new string[] {"-n", "--newline"}, "newline code(cr,lf,crlf)"));
-            command.Add(new Option<string>(new string[] {"-f", "--format"}, "output format(csv,tsv,json,yaml)"));
+            command.Add(new Option<string>(new string[] {"-f", "--text-format"}, "output format(csv,tsv,json,yaml)"));
             command.Add(new Option<string>(new string[] {"--comment-starts-with"}, "comment line letter"));
             command.Add(new Option<string>(new string[] {"--table-name-tag"}, "table name tag"));
             command.Add(new Option<string>(new string[] {"--column-name-tag"}, "column name tag"));
             command.Add(new Option<string>(new string[] {"--column-control-tag"}, "column control tag"));
-            command.Add(new Option<FileInfo>(new string[] {"-c", "--config"}, "config file"));
+            command.Add(new Option<FileInfo>(new string[] {"-c", "--config-file"}, "config file"));
 
-
-            command.Handler = CommandHandler.Create((
-                List<FileInfo> inputFiles,
-                string outputDir,
-                string newline,
-                string format,
-                string commentStartsWith,
-                string tableNameTag,
-                string columnNameTag,
-                string columnControlTag,
-                FileInfo config) =>
-                {
-                    var cfg = new Config();
-
-                    var configFileList = new List<String>() {"config.yml","config.yaml", "Config.yml", "Config.yaml"};
-
-                    if (config != null && config.Exists)
-                    {
-                        cfg = Config.LoadFromFile(config.FullName);
-                    }
-                    else
-                    {
-                        foreach (var file in configFileList)
-                        {
-                            if (File.Exists(file))
-                            {
-                                cfg = Config.LoadFromFile(file);
-                                break;
-                            }
-                        }
-                    }
-                    
-                    if (!String.IsNullOrEmpty(outputDir))
-                        cfg.OutputDir = outputDir;
-                    if (!String.IsNullOrEmpty(newline))
-                        cfg.SetNewLine(newline);
-                    if (!String.IsNullOrEmpty(format))
-                        cfg.SetTextFormat(format);
-                    if (!String.IsNullOrEmpty(commentStartsWith))
-                        cfg.CommentStartsWith = commentStartsWith;
-                    if (!String.IsNullOrEmpty(tableNameTag))
-                        cfg.TableNameTag = tableNameTag;
-                    if (!String.IsNullOrEmpty(columnNameTag))
-                        cfg.ColumnNameTag = columnNameTag;
-                    if (!String.IsNullOrEmpty(columnControlTag))
-                        cfg.ColumnControlTag = columnControlTag;
-                    
-                    Convert(inputFiles.Select(f => f.FullName).ToList(), cfg);
-                });
+            command.Handler = CommandHandler.Create((Config config) =>
+            {
+                config.LoadFromFile();
+                Convert(config);
+            });
             
             command.Invoke(args);
             Console.WriteLine("End");
